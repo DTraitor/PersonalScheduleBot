9e3cc4eec516a179325eb246d1780ace33804c8b
DTraitor <sehhd32@gmail.com>
2025-09-16 10:24:13 +0300

    init changes

--- personalschedulebot/__init__.py
+++ personalschedulebot/__init__.py
@@ -1,1 +1,5 @@
 __version__ = "0.8.0"
+from Lesson import Lesson
+from LessonMessageMapper import generate_telegram_message_from_list, generate_telegram_message
+from ScheduleAPI import get_schedule, user_exists, create_user, change_user_group, get_groups, get_faculties
+from main import main
